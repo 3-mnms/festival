c2529beaf10088bd2e53496fb193c5fe27932eec
정재운 <jaeun101501@gmail.com>
2025-09-01 10:05:14 +0900

    MNMS-447 feat: ai 연결 추가

--- src/main/java/com/teckit/festival/config/WebClientConfig.java
+++ src/main/java/com/teckit/festival/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.teckit.festival.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -8,10 +9,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${ai.url}")
+    private String aiUrl;
+
     @Bean
     public WebClient reviewAiWebClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8084")
+                .baseUrl(aiUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
