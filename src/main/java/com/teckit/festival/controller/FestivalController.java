a1e9ed75281bb6714ea7ab8ef36fad55ad91ae5e
rookies-dogun <rookies.dogun@gmail.com>
2025-09-19 12:14:14 +0900

    MNMS-681 feat: performance test

--- src/main/java/com/teckit/festival/controller/FestivalController.java
+++ src/main/java/com/teckit/festival/controller/FestivalController.java
@@ -170,7 +170,7 @@ public class FestivalController {
 
         int a=0;
         int b=0;
-        for(int i=0; i < 10000; i++){
+        for(int i=0; i < 100000; i++){
            a++;
            b++;
         }
